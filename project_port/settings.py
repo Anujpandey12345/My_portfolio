6e575572fbb574abef98c0ebd5873d61ba373384
anujpandey12345 <your-anujpandey61687@gmail.com>
2025-05-14 00:30:15 +0530

    push kardo

--- project_port/settings.py
+++ project_port/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=21vqnr9s$dyf2pjq0vf3r*aj5yq7$5)lvl=hpt0ofos-z+edw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','127.0.0.1','.now.sh']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -77,11 +77,11 @@ WSGI_APPLICATION = 'project_port.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
+        'NAME': 'portfolio',
         'USER': 'postgres',
-        'PASSWORD': 'kZqDiEqzmeJktfKCFZpiDvtqhGsJmyYO',
-        'HOST': 'tramway.proxy.rlwy.net',
-        'PORT': '29062',
+        'PASSWORD': 'anuj',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -125,7 +125,6 @@ MEDIA_URL = 'media/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR / 'static')
 ]  # if you're using a global static folder
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
